85ee5d7f833420fa393f6b8ad9cc13302b319676
Marco Garcia <macbookpro@MacBook-Pro-de-Macbook.local>
2023-06-07 19:54:58 -0600

    Added pokemon resource

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // agregar prefijo global
+  app.setGlobalPrefix('api/v2')
+
+  /*
+    Dependencia para validaciones globales de los DTO
+  * */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, })
+  );
+
   await app.listen(3000);
 }
 bootstrap( );
